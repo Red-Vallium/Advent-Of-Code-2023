a92ad29df976a94a09f4bc0245cd76b744d9c0a5
Bożek, Piotr <Piotr.Bozek@keyloop.com>
2023-12-01 17:14:00 +0100

    Cookie monster

--- Advent of Code/Program.cs
+++ Advent of Code/Program.cs
@@ -1,12 +1,34 @@
-﻿namespace Advent_of_Code
+﻿using System.Net;
+
+namespace Advent_of_Code
 {
     internal class Program
     {
+        private const string Url = "https://adventofcode.com/2023/day/1/input";
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var cookieContainer = new CookieContainer();
+            var baseUri = new Uri(Url); 
+
+            cookieContainer.Add(baseUri, new Cookie("session", "YourSessionToken"));
+            
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookieContainer
+            };
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                try
+                {
+                    var response = await client.GetStringAsync(Url);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
         }
-
-
     }
 }
